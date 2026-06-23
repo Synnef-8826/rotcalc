2e8b6210f683bf10a45723f986d51ea5dd4d1153
Synnef <106876032+Synnef-8826@users.noreply.github.com>
2026-06-23 14:47:15 +0000

    Add files via upload

--- rotcalc/rotcalc.py
+++ rotcalc/rotcalc.py
@@ -1,20 +1,24 @@
 import argparse, sys, json, os, lib
 
+
 # Initial setup
 if "settings.json" not in os.listdir("data"): lib.loadDefaults()
 
+
 argList = [ # short,long,desc,type,options?
         ["-r", "--run", "run rotcalc with the current settings", "store_true"],
+        ["-m", "--manual", "run rotcalc in manual mode with the current dataset, meant to quickly preview leveling progress or individual rotations", "store_true"],
         ["-o", "--output", "send all output to file OUTPUT", "store"],
         ["-s", "--settings", "print current settings", "store_true"],
 
-        ["-d", "--dataset", "change the active dataset (must be a filename in ./data/datasets, e.g 'Cheesecake.json')", "store"],
+        ["-d", "--dataset", "change the active dataset (must be a filename in ./data/datasets, e.g 'example.json')", "store"],
         ["-a", "--amount", "set amount of rotations to generate", "store"],
         ["-c", "--count", "set length of generated rotations, 9/10 recommended for infamy", "store"],
-        ["-p", "--penalty", "toggles generating rotations that don't prevent heat penalties'", "store_true"],
+        ["-p", "--penalty", "toggles generating rotations that don't prevent heat penalties", "store_true"],
         ["-g", "--goal", "change criteria for selecting best rotations", "store", ["xp", "time"]],
         ["-l", "--load", "specify a load time that will be added between every heist, use for approximating RTA timing", "store"],
 ]
+
 
 # Add arguments defined by argList
 parser = argparse.ArgumentParser(description="Tool for generating PAYDAY 2 heist rotations")
@@ -23,12 +27,14 @@ for arg in argList:
                 case 4: parser.add_argument(arg[0], arg[1], help=arg[2], action=arg[3])
                 case 5: parser.add_argument(arg[0], arg[1], help=arg[2], action=arg[3], choices=arg[4])
 
+
 # Exit and print help if no arguments are provided
 if len(sys.argv) == 1:
         parser.print_help()
         exit()
 
 args = parser.parse_args()
+
 
 # --- Settings arguments ---
 if args.dataset:
@@ -56,11 +62,17 @@ if args.load:
         lib.printSettingUpdate("loadTime", lib.getSettings()["loadTime"], args.count)
         lib.changeSetting("loadTime", int(args.load))
 
+
 # --- Info arguments ---
 if args.settings:
         for setting in lib.getSettings(): print(f"{setting}: {lib.getSettings()[setting]}")
+
 
 # --- Run arguments ---
 if args.run:
         from calculator import generateRotations, plotRotation
         generateRotations(lib.getSettings()["amount"], lib.getSettings()["heistCount"], not lib.getSettings()["allowPenalties"], lib.getSettings()["loadTime"], lib.getSettings()["goal"], args.output)
+
+if args.manual:
+        from calculator import manualPlotting
+        manualPlotting()
